0ab37de16c27c40cf0f3b418f6281a0a288c14d5
Lucas Felix <lfelixsampaio@gmail.com>
2017-06-30 04:25:56 -0300

    modulo 3 - aula 1

--- javaee-m3a1/src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
+++ javaee-m3a1/src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
@@ -55,14 +55,3 @@ public class LivroDao {
 	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-
--- javaee-m3a1/src/main/java/br/com/casadocodigo/loja/models/Livro.java
+++ javaee-m3a1/src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -18,94 +18,120 @@ import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Cacheable
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
 public class Livro {
 
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@NotBlank
 	private String titulo;
-	
+
 	@Lob
-	@Length(min=10)
+	@Length(min = 10)
 	@NotBlank
 	private String descricao;
-	
+
 	@DecimalMin("20")
 	private BigDecimal preco;
 	@Min(50)
 	private Integer numeroPaginas;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Calendar dataPublicacao;
-	
+
 	private String capaPath;
-	
+
 	@ManyToMany
-	@Size(min=1)
+	@Size(min = 1)
 	@NotNull
+	@XmlElement(name = "autor")
+	@XmlElementWrapper(name = "autores")
 	private List<Autor> autores = new ArrayList<>();
-	
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public BigDecimal getPreco() {
 		return preco;
 	}
+
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
+
 	public Integer getNumeroPaginas() {
 		return numeroPaginas;
 	}
+
 	public void setNumeroPaginas(Integer numeroPaginas) {
 		this.numeroPaginas = numeroPaginas;
 	}
+
 	public List<Autor> getAutores() {
 		return autores;
 	}
+
 	public void setAutores(List<Autor> autores) {
 		this.autores = autores;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Livro [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco
 				+ ", numeroPaginas=" + numeroPaginas + ", autores=" + autores + "]";
 	}
+
 	public Calendar getDataPublicacao() {
 		return dataPublicacao;
 	}
+
 	public void setDataPublicacao(Calendar dataPublicacao) {
 		this.dataPublicacao = dataPublicacao;
 	}
+
 	public String getCapaPath() {
 		return capaPath;
 	}
+
 	public void setCapaPath(String capaPath) {
 		this.capaPath = capaPath;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -113,6 +139,7 @@ public class Livro {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -130,12 +157,3 @@ public class Livro {
 		return true;
 	}
 }
-
-
-
-
-
-
-
-
-
